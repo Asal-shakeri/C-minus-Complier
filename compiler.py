5be306a096985b07631815b6bc9652b570524412
MohammadYSF <aa.yosefiyan7@gmail.com>
2025-08-15 02:40:23 +0330

    change

--- compiler.py
+++ compiler.py
@@ -3,13 +3,14 @@ from Parser.parser import LL1
 from scanner.default_scanner import build_scanner
 from scanner.tokens import Token, TokenType
 from tables import tables
-
+from code_gen import CodeGen
 
 
 
 tables.symbol_table.add_symbol(Token(TokenType.ID, "output"))
 tables.symbol_table.fetch("output").address = 5
-parser = LL1(build_scanner("input.txt"), init_grammar())
+tables.symbol_table.export("symbol_table.txt")
+parser = LL1(build_scanner("input.txt"), init_grammar(),CodeGen())
 parser.generate_parse_tree()
 parser.export_parse_tree("parse_tree.txt")
 parser.export_syntax_error("syntax_errors.txt")
